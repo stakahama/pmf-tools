5b678b29c0050b49e88fb5567e9e97c503a8f332
satoshi <stakahama@0-25-4b-87-e9-8b.dynamic.ucsd.edu>
2011-06-06 14:56:33 -0700

    added batch run scripts

---
+++ ftir-batch-run.r
@@ -0,0 +1,10 @@
+postprocess <- function() {
+  source("pmf-qvalues.r",local=TRUE)
+  source("pmf-mutual-correlations.r",local=TRUE)    
+  source("pmf-explvar.r",local=TRUE)      
+  source("pmf-explvar-plots.r",local=TRUE)
+}
+
+postprocess()
+system("Rscript pmf-g-correlations.r")
+system("Rscript ftir-factor-classification.r")        
